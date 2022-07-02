3a3cb82b77c84fb86e560b2ad4e1f7f05ff0a1ca
Thushan <thushan.perera95@gmail.com>
2022-07-03 00:25:13 +0800

    Updated pipeline to deploy to heroku

--- backend/app.js
+++ backend/app.js
@@ -25,6 +25,10 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 app.use(middleware.tokenExtractor)
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
