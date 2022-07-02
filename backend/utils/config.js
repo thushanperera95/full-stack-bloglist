87cb263e8e24b8bec923d7040088fb1e98fedb49
Thushan <thushan.perera95@gmail.com>
2022-07-02 15:33:08 +0800

    Added couple of console logs

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -7,6 +7,9 @@ const MONGODB_URI = process.env.NODE_ENV === 'test'
   ? process.env.TEST_MONGODB_URI
   : process.env.MONGODB_URI
 
+console.log(PORT)
+console.log(MONGODB_URI)
+
 module.exports = {
   MONGODB_URI,
   PORT
